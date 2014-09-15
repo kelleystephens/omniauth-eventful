33722db49e8250c21a8c33ebf42eb39029b63710
Kelley Stephens <kelley.stephens@outlook.com>
2014-09-15 15:32:10 -0500

    authentication through Eventful functioning

--- lib/omniauth/strategies/eventful.rb
+++ lib/omniauth/strategies/eventful.rb
@@ -1,46 +1,41 @@
-require 'omniauth-oauth2'
+require 'omniauth-oauth'
+require "multi_xml"
 
 module OmniAuth
   module Strategies
-    class Eventful
-      include OmniAuth::Strategy
+    class Eventful < OmniAuth::Strategies::OAuth
+
+      args [:consumer_key, :consumer_secret, :app_key]
 
       option :name, "eventful"
+      option :consumer_key, nil
+      option :consumer_secret, nil
+      option :app_key, nil
 
       option :client_options, {
         site: "http://api.eventful.com",
+        request_token_url: "http://eventful.com/oauth/request_token",
         authorize_url: "http://eventful.com/oauth/authorize",
-        token_url: "http://eventful.com/oauth/request_token"
+        access_token_url: "http://eventful.com/oauth/access_token"
       }
 
-      def request_phase
-        super
-      end
-
-      def authorize_params
-        super.tap do |params|
-          %w[scope client_options].each do |v|
-            if request.params[v]
-              params[v.to_sym] = request.params[v]
-            end
-          end
-        end
-      end
-
-      def callback_url
-        options[:redirect_uri] || super
-      end
-
-      uid { raw_info["id"].to_s }
+      uid{ raw_info["user"]["username"] }
 
       info do
+        name = [raw_info["user"]["first_name"], raw_info["user"]["last_name"]].compact.join(' ').strip
         {
+          raw_info: raw_info,
+          name: name,
           username: raw_info["user"]["username"],
           bio: raw_info["user"]["bio"],
           hometown: raw_info["user"]["hometown"],
           first_name: raw_info["user"]["first_name"],
           last_name: raw_info["user"]["last_name"],
-          interests: raw_info["user"]["interests"]
+          interests: raw_info["user"]["interests"],
+          images: raw_info["user"]["images"],
+          links: raw_info["user"]["links"],
+          locales: raw_info["user"]["locales"],
+          going: raw_info["user"]["going"]
         }
       end
 
@@ -49,8 +44,7 @@ module OmniAuth
       end
 
       def raw_info
-        access_token.options[:mode] = :query
-        @raw_info ||= access_token.get('user').parsed
+        @raw_info ||= MultiXml.parse(access_token.get("http://api.eventful.com/rest/users/get?app_key=#{options.app_key}&oath_consumer_key=#{options.consumer_key}").body)
       end
     end
   end
--- spec/omniauth/strategies/eventful_spec.rb
+++ spec/omniauth/strategies/eventful_spec.rb
@@ -1,37 +1,143 @@
 require 'spec_helper'
-require 'pry'
 
-describe OmniAuth::Strategies::Eventful do
-  let(:access_token) { double('AccessToken', :options => {}) }
-  let(:parsed_response) { double('ParsedResponse') }
-  let(:response) { double('Response', :parsed => parsed_response) }
-
-  subject do
-    OmniAuth::Strategies::Eventful.new({})
+describe "OmniAuth::Strategies::Eventful" do
+  class Eventful < OmniAuth::Strategies::OAuth
+    uid{ access_token.token }
+    info{ {'name' => access_token.token} }
   end
 
-  before(:each) do
-    allow(subject).to receive(:access_token) { access_token }
+  def app
+    Rack::Builder.new {
+      use OmniAuth::Test::PhonySession
+      use OmniAuth::Builder do
+        provider Eventful, 'abc', 'def', :client_options => {:site => 'https://api.eventful.com'}, :name => 'eventful.com'
+        provider Eventful, 'abc', 'def', :client_options => {:site => 'https://api.eventful.com'}, :authorize_params => {:abc => 'def'}, :name => 'eventful.com_with_authorize_params'
+        provider Eventful, 'abc', 'def', :client_options => {:site => 'https://api.eventful.com'}, :request_params => {:scope => 'http://foobar.eventful.com'}, :name => 'eventful.com_with_request_params'
+      end
+      run lambda { |env| [404, {'Content-Type' => 'text/plain'}, [env.key?('omniauth.auth').to_s]] }
+    }.to_app
   end
 
-  context "client options" do
-    it 'should have correct site' do
-      subject.options.client_options.site.should eq("http://api.eventful.com")
+  def session
+    last_request.env['rack.session']
+  end
+
+  before do
+    stub_request(:post, 'https://api.eventful.com/oauth/request_token').
+       to_return(:body => "oauth_token=yourtoken&oauth_token_secret=yoursecret&oauth_callback_confirmed=true")
+  end
+
+  describe '/auth/{name}' do
+    context 'successful' do
+      before do
+        get '/auth/eventful.com'
+      end
+
+      it 'should redirect to authorize_url' do
+        last_response.should be_redirect
+        last_response.headers['Location'].should == 'https://api.eventful.com/oauth/authorize?oauth_token=yourtoken'
+      end
+
+      it 'should redirect to authorize_url with authorize_params when set' do
+        get '/auth/eventful.com_with_authorize_params'
+        last_response.should be_redirect
+        [
+          'https://api.eventful.com/oauth/authorize?abc=def&oauth_token=yourtoken',
+          'https://api.eventful.com/oauth/authorize?oauth_token=yourtoken&abc=def'
+        ].should be_include(last_response.headers['Location'])
+      end
+
+      it 'should set appropriate session variables' do
+        session['oauth'].should == {"eventful.com" => {'callback_confirmed' => true, 'request_token' => 'yourtoken', 'request_secret' => 'yoursecret'}}
+      end
+
+      it 'should pass request_params to get_request_token' do
+        get '/auth/eventful.com_with_request_params'
+        WebMock.should have_requested(:post, 'https://api.eventful.com/oauth/request_token').
+           with {|req| req.body == "scope=http%3A%2F%2Ffoobar.eventful.com" }
+      end
     end
 
-    it 'should have correct authorize url' do
-      subject.options.client_options.authorize_url.should eq('http://eventful.com/oauth/authorize')
-    end
+    context 'unsuccessful' do
+      before do
+        stub_request(:post, 'https://api.eventful.com/oauth/request_token').
+           to_raise(::Net::HTTPFatalError.new(%Q{502 "Bad Gateway"}, nil))
+        get '/auth/eventful.com'
+      end
 
-    it 'should have correct token url' do
-      subject.options.client_options.token_url.should eq('http://eventful.com/oauth/request_token')
+      it 'should call fail! with :service_unavailable' do
+        last_request.env['omniauth.error'].should be_kind_of(::Net::HTTPFatalError)
+        last_request.env['omniauth.error.type'] = :service_unavailable
+      end
+
+      context "SSL failure" do
+        before do
+          stub_request(:post, 'https://api.eventful.com/oauth/request_token').
+             to_raise(::OpenSSL::SSL::SSLError.new("SSL_connect returned=1 errno=0 state=SSLv3 read server certificate B: certificate verify failed"))
+          get '/auth/eventful.com'
+        end
+
+        it 'should call fail! with :service_unavailable' do
+          last_request.env['omniauth.error'].should be_kind_of(::OpenSSL::SSL::SSLError)
+          last_request.env['omniauth.error.type'] = :service_unavailable
+        end
+      end
     end
   end
 
-  context "#raw_info" do
-    it "should use relative paths" do
-      access_token.should_receive(:get).with('user').and_return(response)
-      subject.raw_info.should eq(parsed_response)
+    describe '/auth/{name}/callback' do
+      before do
+        stub_request(:post, 'https://api.eventful.com/oauth/access_token').
+         to_return(:body => "oauth_token=yourtoken&oauth_token_secret=yoursecret")
+      get '/auth/eventful.com/callback', {:oauth_verifier => 'dudeman'}, {'rack.session' => {'oauth' => {"eventful.com" => {'callback_confirmed' => true, 'request_token' => 'yourtoken', 'request_secret' => 'yoursecret'}}}}
+    end
+
+    it 'should exchange the request token for an access token' do
+      last_request.env['omniauth.auth']['provider'].should == 'eventful.com'
+      last_request.env['omniauth.auth']['extra']['access_token'].should be_kind_of(OAuth::AccessToken)
+    end
+
+    it 'should call through to the master app' do
+      last_response.body.should == 'true'
+    end
+
+    context "bad gateway (or any 5xx) for access_token" do
+      before do
+        stub_request(:post, 'https://api.eventful.com/oauth/access_token').
+           to_raise(::Net::HTTPFatalError.new(%Q{502 "Bad Gateway"}, nil))
+        get '/auth/eventful.com/callback', {:oauth_verifier => 'dudeman'}, {'rack.session' => {'oauth' => {"eventful.com" => {'callback_confirmed' => true, 'request_token' => 'yourtoken', 'request_secret' => 'yoursecret'}}}}
+      end
+
+      it 'should call fail! with :service_unavailable' do
+        last_request.env['omniauth.error'].should be_kind_of(::Net::HTTPFatalError)
+        last_request.env['omniauth.error.type'] = :service_unavailable
+      end
+    end
+
+    context "SSL failure" do
+      before do
+        stub_request(:post, 'https://api.eventful.com/oauth/access_token').
+           to_raise(::OpenSSL::SSL::SSLError.new("SSL_connect returned=1 errno=0 state=SSLv3 read server certificate B: certificate verify failed"))
+        get '/auth/eventful.com/callback', {:oauth_verifier => 'dudeman'}, {'rack.session' => {'oauth' => {"eventful.com" => {'callback_confirmed' => true, 'request_token' => 'yourtoken', 'request_secret' => 'yoursecret'}}}}
+      end
+
+      it 'should call fail! with :service_unavailable' do
+        last_request.env['omniauth.error'].should be_kind_of(::OpenSSL::SSL::SSLError)
+        last_request.env['omniauth.error.type'] = :service_unavailable
+      end
+    end
+  end
+
+  describe '/auth/{name}/callback with expired session' do
+    before do
+      stub_request(:post, 'https://api.eventful.com/oauth/access_token').
+         to_return(:body => "oauth_token=yourtoken&oauth_token_secret=yoursecret")
+      get '/auth/eventful.com/callback', {:oauth_verifier => 'dudeman'}, {'rack.session' => {}}
+    end
+
+    it 'should call fail! with :session_expired' do
+      last_request.env['omniauth.error'].should be_kind_of(::OmniAuth::NoSessionError)
+      last_request.env['omniauth.error.type'] = :session_expired
     end
   end
 end
